3a37b022960e610d811975ffc09f336faf4cf7ce
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-19 13:01:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,13 @@
 class Song
   attr_accessor :name,:artist,:genre
-@@count = 0
+  @@count = 0
 
 def initialize(name,artist,genre)
   @name = name
   @artist = artist
   @genre = genre
    @@count += 1
- end
- end
+end
 
 end
 
@@ -21,11 +20,3 @@ end
 
 
 
-
-
-def initialize
-   @@count += 1
- end
-
- def self.count
-   @@count
